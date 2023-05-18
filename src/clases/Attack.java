8d7a8e50002160ca94b735963b45a38787699262
Genesis Perez <genesis.perez@accenture.com>
2023-05-18 00:55:46 -0300

    Pokemon class created

---
+++ src/clases/Attack.java
@@ -0,0 +1,6 @@
+package clases;
+
+//Creating the abstract class that is going to be used by the different tasks.
+public abstract class Attack {
+    
+}
---
+++ src/clases/Pokemon.java
@@ -0,0 +1,48 @@
+package clases;
+
+public class Pokemon {
+    int pokedex_num_pokemon;
+    String name_pokemon;
+    double weight_pokemon;
+    int season_pokemon;
+
+    public Pokemon() {
+
+    }
+
+    public Pokemon(int pokedex_num_pokemon, String name_pokemon, double weight_pokemon, int season_pokemon) {
+
+        this.pokedex_num_pokemon = pokedex_num_pokemon;
+        this.name_pokemon = name_pokemon;
+        this.weight_pokemon = weight_pokemon;
+        this.season_pokemon = season_pokemon;
+    }
+
+    public void setPokedexNumberPokemon(int pokedex_num_pokemon) {
+        this.pokedex_num_pokemon = pokedex_num_pokemon;
+    }
+
+    public int getNPokedexNumberPokemon() {
+        return pokedex_num_pokemon;
+    }
+
+    public void setNamePokemon(String name_pokemon) {
+        this.name_pokemon = name_pokemon;
+    }
+
+    public String getNamePokemon() {
+        return name_pokemon;
+    }
+
+    public void setWeight(double weight_pokemon) {
+        this.weight_pokemon = weight_pokemon;
+    }
+
+    public double getWeightPokemon() {
+        return weight_pokemon;
+    }
+
+    public void setSeasonPokemon(int season_pokemon) {
+        this.season_pokemon = season_pokemon;
+    }
+}
